99882e64506427cb97725a0d177d61905a0fb752
imbarilyn <linahmuhonjaimbari@gmail.com>
2023-02-11 07:16:52 +0300

    add data object

---
+++ src/Helpers/data.js
@@ -0,0 +1,46 @@
+import carental from '../assets/carental.jpg';
+import expense from '../assets/expense.jpg';
+import pizza from '../assets/pizza.jpg';
+import locate from '../assets/locate.jpg';
+import bank from '../assets/bank.jpg';
+import locate2 from '../assets/locate2.jpg'
+
+
+
+ export const data = [
+    {
+        name: "Kuloketi Divaisi",
+        image: locate2,
+        github: "https://github.com/imbarilyn/Kuloketi-Divaisi",
+        demo: "https://imbarilyn.github.io/Kuloketi-Divaisi/",
+        description: "This application helps a user to locate which county they are at by a press on a button"
+    },
+    {
+        name: "Expense Calculator",
+        image: expense,
+        github: "https://github.com/imbarilyn/expense-calculator-device",
+        demo: "https://expense-calculator-device.vercel.app/",
+        description: "Expense calculator helps a user to keep a record of their expenditure"
+    }, 
+    {
+        name: "Pizza Shop",
+        image: pizza,
+        github: "https://github.com/imbarilyn/Pizza-Shop",
+        demo: "https://pizza-yetu-sote.onrender.com/",
+        description: "Pizza Shop is a platform for making reviews for a given pizza"
+    },
+    {
+        name: "Flatiron Bank",
+        image: bank,
+        github: "https://github.com/imbarilyn/Bank-of-Flatiron",
+        demo: "https://imbarilyn.github.io/Bank-of-Flatiron/",
+        description: "An application that simulates a bank application"
+    },
+    {
+        name: "Car Rental",
+        image: carental,
+        github: "https://github.com/imbarilyn/carreview",
+        demo: "https://sean-code.github.io/cars/",
+        description: "An application for car renting and bookings"
+    }
+]
